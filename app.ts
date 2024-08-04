d4ffa942d0232fd8c25289497c081448b8a43d8d
OstrichWhite <rahimcsestudies@gmail.com>
2024-08-04 20:29:54 +0530

    Added middleware to parse the req.body.

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import router from "./routes";
 
 const app = express();
 
+app.use(express.json())
 app.use("/api/v1", router)
 app.get("/ping", (req, res) => res.json({message: "Hello from server."}))
 
