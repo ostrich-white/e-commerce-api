8dc13e3e697447da7e561ac048df391a4e10ec43
OstrichWhite <rahimcsestudies@gmail.com>
2024-08-04 09:28:15 +0530

    Fixed logging issues.

--- index.ts
+++ index.ts
@@ -5,4 +5,4 @@ const port = 8000
 
 app.get("/ping", (req, res) => res.json({message: "Hello from server."}))
 
-app.listen(port, ()=> `Server listening on port ${port}.`)
+app.listen(port, () => console.log(`Server listening on port ${port}.`))
