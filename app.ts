64551a3f3b2aa27ee4f6762c66ad8961e7127cc1
OstrichWhite <rahimcsestudies@gmail.com>
2024-08-10 03:42:45 +0530

    Introduced routes.

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import router from "./routes";
 
 const app = express();
 
+app.use("/api/v1", router)
 app.get("/ping", (req, res) => res.json({message: "Hello from server."}))
 
 export default app
---
+++ routes/index.ts
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+
+const router = Router()
+// Enter your routes here to bind.
+
+export default router
