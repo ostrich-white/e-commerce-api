af0b0c6047c47641aa4430cb013b515db05164da
OstrichWhite <rahimcsestudies@gmail.com>
2024-08-10 07:10:37 +0530

    Refined error structure.

--- controllers/appErrors.ts
+++ controllers/appErrors.ts
@@ -5,5 +5,10 @@ export default (err: IAppError, req: Request, res: Response, next: NextFunction)
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
   
-    res.status(err.statusCode).json({message: "Something went wrong.", err})
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
   };
