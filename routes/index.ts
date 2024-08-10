9666b05d975fbc3aa849550d32fcee7961d7c20c
OstrichWhite <rahimcsestudies@gmail.com>
2024-08-11 04:37:45 +0530

    Bind product route.

--- routes/index.ts
+++ routes/index.ts
@@ -1,6 +1,9 @@
+import products from "./products";
+
 const { Router } = require("express");
 
 const router = Router()
 // Enter your routes here to bind.
+router.use("/products", products)
 
 export default router
