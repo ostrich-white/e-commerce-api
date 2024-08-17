1eaa3f6e9ef31535addc9b31a6e9c13aeaf1de8b
durai-murugan-2262 <havocdurai67@gmail.com>
2024-08-17 20:51:22 +0530

    Added body-parser.

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import router from "./routes";
 
 const app = express();
 
+app.use(express.json())
 app.get("/ping", (req, res) => res.json({ message: "Hello from server." }))
 app.use('/api/v1', router)
 
