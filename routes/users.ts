aa5462987802da8e453bb3f3dbb5b7d2e365d097
durai-murugan-2262 <havocdurai67@gmail.com>
2024-08-17 20:57:48 +0530

    Added signup route to users.

--- routes/users.ts
+++ routes/users.ts
@@ -3,4 +3,16 @@ import users, { User } from "../models/users";
 
 const router = Router()
 
+router.post('/signup', async (req, res) => {
+    try {
+        const createdUser = await users.create(req.body)
+        
+        res.status(201).json({ message: "User created successfully", "data": createdUser })
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({ message: 'Error creating user', error: error })
+    }
+})
+
 export default router
