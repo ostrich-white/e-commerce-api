8f14597b5454e6e7558ecc57c27ff5af32bddeea
OstrichWhite <rahimcsestudies@gmail.com>
2024-08-18 05:54:31 +0530

    Configured cors.

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes";
 import appErrors from "./controllers/appErrors";
 
 const app = express();
 
+app.use(cors({
+    origin: "*"
+}))
 app.use(express.json())
 
 app.use("/api/v1", router)
