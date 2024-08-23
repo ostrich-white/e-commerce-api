37fb8e66a3a9f1f0ec53fd0d694749ba2af487aa
durai-murugan-2262 <havocdurai67@gmail.com>
2024-08-23 08:54:07 +0530

    Renamed users routes to auth and added protect middleware.

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,21 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import users from "../models/users";
 import jwt from "jsonwebtoken";
 
+const jwtSecretKey  = process.env.JWT_SECRET_KEY || 'secretKey'
 const router = Router()
+
+export const protect = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization && req.headers.authorization.split(" ")[1]
+    if(!token)
+        return res.status(401).json({message: "No token, unauthorized access"})
+    try {
+        req.user = jwt.verify(token, jwtSecretKey)
+        next()
+    } catch (error) {
+        res.status(401).json({error})
+    }
+}
 
 router.post('/signup', async (req, res) => {
     try {
@@ -22,13 +35,13 @@ router.post('/login', async ({body: {email, password}}, res) => {
         
         if(!user || !(await user.matchPassword(password)))
             return res.status(400).json({message: "Invalid credentials"})
-        const token = jwt.sign({id: user._id}, 'jwt_secret_key', {expiresIn: '1h'})
+        const token = jwt.sign({id: user._id}, jwtSecretKey, {expiresIn: '1h'})
         res.status(200).json({
             message: "user logged in successfully.",
             token: token
         })
     } catch (error) {
-        res.status(403).json({error: error})
+        res.status(403).json({error})
     }
 })
 
--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import userRouter from "./users";
+import authRouter from "./auth";
 
 const router = Router()
 
-router.use('/users', userRouter)
+router.use('/auth', authRouter)
 
 export default router
