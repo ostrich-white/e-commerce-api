de52ac62750bca8f3706c74ad3101f4fc923d486
durai-murugan-2262 <havocdurai67@gmail.com>
2024-08-26 22:46:33 +0530

    Added cookieParser in app.

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import router from "./routes";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
 app.get("/ping", (req, res) => res.json({ message: "Hello from server." }))
 app.use('/api/v1', router)
 
