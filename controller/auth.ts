c06f2b05610fc79176a824659359b9a05f3d5227
durai-murugan-2262 <havocdurai67@gmail.com>
2024-08-27 21:34:14 +0530

    Added forget-password and reset-password in auth router.

--- controller/auth.ts
+++ controller/auth.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import users from "../models/users";
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const jwtSecretKey  = process.env.JWT_SECRET_KEY || 'secretKey'
 
@@ -37,5 +38,55 @@ export const login = async ({body: {email, password}}: Request, res: Response) =
         res.redirect('/api/v1/users/getme');
     } catch (error) {
         res.status(403).json({error})
+    }
+}
+
+export const forgetPassword = async (req: Request, res: Response) => {
+    const {body: {email}} = req;
+    try {
+        const user = await users.findOne({email})
+        
+        if(!user)
+            return res.status(404).json({message: "User not found"})
+        
+        const resetToken = crypto.randomBytes(32).toString('hex')
+        const hashedToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+
+        user.resetPasswordToken = hashedToken
+        user.tokenExpiresAt = Date.now() + 10 * 60 * 1000
+        // Send email with reset password link
+        const resetURL = `${req.protocol}://${req.get('host')}/api/v1/auth/reset-password/${resetToken}`
+        console.log('password reset link ' + resetURL)
+
+        await user.save()
+        res.status(200).json({message: "Password reset link sent"})
+    } catch (error) {
+        res.status(500).json({message: 'Error resetting password', error: error })
+    }
+}
+
+export const resetPassword = async (req: Request, res: Response) => {
+    const {token} = req.params;
+    const {body: {password, confirmPassword}} = req;
+
+    try {
+        const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+
+        const user = await users.findOne({
+            resetPasswordToken: hashedToken,
+            tokenExpiresAt: {$gt: Date.now()}
+        });
+
+        if(!user)
+            return res.status(400).json({message: "Invalid or expired token"})
+
+        user.password = password
+        user.resetPasswordToken = ''
+        user.tokenExpiresAt = 0
+        await user.save()
+
+        res.status(200).json({message: "Password reset successful"})
+    } catch (error) {
+        res.status(500).json({ error: error })
     }
 }
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, signup } from "../controller/auth";
+import { forgetPassword, login, resetPassword, signup } from "../controller/auth";
 
 const router = Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
+router.post('/forget-password', forgetPassword)
+router.post('/reset-password/:token', resetPassword)
 
-export default router
+export default router;
