6df299a774ce21ead105f40f2c65b269d8d53f03
durai-murugan-2262 <havocdurai67@gmail.com>
2024-09-05 22:46:16 +0530

    Introduced product router.

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth";
 import userRouter from "./user"
+import productRouter from "./product";
 
 const router = Router()
 
 router.use('/auth', authRouter)
 router.use('/users', userRouter)
+router.use('/products', productRouter)
 
 export default router
