e96c5ed3adb537cbe96c140edae4aca4825d9be8
durai-murugan-2262 <havocdurai67@gmail.com>
2024-09-10 08:52:13 +0530

    Moved protect to users router.

--- routes/index.ts
+++ routes/index.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import authRouter from "./auth";
 import userRouter from "./user"
 import productRouter from "./product";
+import { protect } from "../controller/auth";
 
 const router = Router()
 
 router.use('/auth', authRouter)
-router.use('/users', userRouter)
+router.use('/users', protect, userRouter)
 router.use('/products', productRouter)
 
 export default router
--- routes/user.ts
+++ routes/user.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { create, get, getAll, getMe, update } from "../controller/user";
-import { authorize, protect } from "../controller/auth";
+import { authorize } from "../controller/auth";
 
 const router = Router()
 
-router.get('/getme', protect, getMe)
-router.get('/:id', protect, get)
-router.get('/', protect, authorize('admin'), getAll)
-router.post('/', protect, authorize('admin'), create)
-router.put('/:id', protect, update)
+router.get('/getme', getMe)
+router.get('/:id', get)
+router.get('/', authorize('admin'), getAll)
+router.post('/', authorize('admin'), create)
+router.put('/:id', update)
 
 export default router;
